fe112dd22c52f8cec3bf11a8e42c26095b2ed47d
Talisa Erazo <talisaerazo@gmail.com>
2018-10-15 19:10:09 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,10 +47,6 @@ def invalid_command
   puts "Please enter a valid command."
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
 def runner
   welcome
   card_total = initial_round
